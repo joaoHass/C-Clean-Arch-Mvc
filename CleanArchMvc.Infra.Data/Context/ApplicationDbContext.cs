c7f32523eecd4cb66aa3772a4bd46b9102480eaa
joaoHass <joaopedrohass@outlook.com>
2022-09-02 05:37:06 -0300

    feat: Criado a configuração do DbContext

---
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchMvc.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Vasculha o assembly e aplica as configurações de todas as classes que herdam da entidade especificada
+            // Ou seja, ele aplica todas as Configurations que criar na pasta EntitiesConfiguration, de forma automática
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            // Se tivesse que fazer na mão (para todos os configurations):
+            // builder.ApplyConfiguration(new CategoryInformation());
+        }
+    }
+}
